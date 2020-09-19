0b91cff0f6b427cea1100784cab4ee27f3984ed4
kevx514 <kevx514@gmail.com>
2020-09-18 20:30:07 -0400

    did the sign-in/sign-up

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
-Vue.config.productionTip = false
+library.add(faUserSecret);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
+
+global.jQuery = require("jquery");
+var $ = global.jQuery;
+window.$ = $;
