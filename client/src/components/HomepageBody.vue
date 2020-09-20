7b4a31571accbf5154b19a74d46cf6bf423a364e
Aaron Walsdorf <awalsdorf1@pride.hostrea.edu>
2020-09-20 18:31:02 -0400

    fixed some text

--- client/src/components/HomepageBody.vue
+++ client/src/components/HomepageBody.vue
@@ -6,7 +6,7 @@
 <p style="text-align:right"> BarterX does allows Hofstra students to trade items of all types with each other. </p> 
 -->
 
-<h2>This is the homepage. Our program, BarterX allows Hofstra students to trade items of all types with each other.  </h2>
+<h2>BarterX allows Hofstra students to trade items of all types with each other.  </h2>
 <br>
 
 <h2>We provide a money back guarantee if any products are not recieved as advertised. </h2>
@@ -16,8 +16,10 @@
 <img src= "https://m.media-amazon.com/images/I/61u1IR857AL._AC_SS350_.jpg"/>
 
 <h3>TEXTBOOKS </h3>
-<img src= "bioTextbook.jpg"/>
+<img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKTorzfwB8rhlSHTEEZzkIoT9cDjW8u-KUERwXMozYcw3F7T8vP8yGgPdT0UUD4Ck3prjHVSPt&usqp=CAc"/>
 </div>
+
+<br> <br>
 </div>
 
 
